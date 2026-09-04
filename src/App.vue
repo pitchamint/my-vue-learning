c39863a9367d900f69e65ed449590cb8307146bf
Pitchamint <>
2026-09-04 17:13:30 +0700

    [UPDATE] v-bing

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,8 @@ const firstName = ref('John')
 const lastName = ref('Doe')
 const age = ref(20)
 const gender = ref('<i>Male</i>')
+const picture = ref('https://www.flaticon.com/free-icon/proud_10623149?related_id=10623149')
+const size = ref(150)
 
 const getFullName = () => {
   return `${firstName.value} ${lastName.value}`
@@ -14,9 +16,9 @@ const getFullName = () => {
  
 <template>
   <section> 
+    <img :src="picture" :width="size" :height="size" />
     <p>ชื่อ - สกุล : {{ getFullName() }} อายุ :{{ age }} ปี</p> 
     <p>เพศ <span v-html="gender"></span></p>
-    
   </section>
 </template>
 
