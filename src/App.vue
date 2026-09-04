075435a424e4cfcc3a17e5157031ff219ab9a083
Pitchamint <>
2026-09-04 15:30:59 +0700

    [UPDATE] learning method

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,25 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue'
+
+const firstName = ref('John')
+const lastName = ref('Doe')
+const age = ref(20)
+const gender = ref('<i>Male</i>')
+
+const getFullName = () => {
+  return `${firstName.value} ${lastName.value}`
+}
+
 </script>
-
+ 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <section> 
+    <p>ชื่อ - สกุล : {{ getFullName() }} อายุ :{{ age }} ปี</p> 
+    <p>เพศ <span v-html="gender"></span></p>
+    
+  </section>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
